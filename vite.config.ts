f1de15f35c5573039a4cb6603f6c879bbd8e927a
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2026-02-26 14:53:02 +0100

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tailwindcss from "@tailwindcss/vite";
+// import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import jsxSource from "unplugin-jsx-source/vite";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    tsconfigPaths(),
-    tanstackStart({
-      srcDirectory: 'src',
-    }),
-    react(),
-  ],
-  server: {
-    port: 5173,
+const defaultTransformFileName = (
+  id: string,
+  loc: {
+    start: { line: number; column: number };
+    end: { line: number; column: number };
   },
-})
+) => {
+  const fileName = id.split("/").slice(-2).join("/") ?? "unknown";
+  return `${fileName}:${loc.start.line}`;
+};
+
+const config = defineConfig((env) => ({
+  resolve: {
+    alias: {
+      // "@dadabase/effect-pglite": "/packages/effect-pglite/src/mod.ts",
+    },
+  },
+  plugins: [
+    // devtools({
+    // 	injectSource: { enabled: false },
+    // 	enhancedLogs: { enabled: false },
+    // 	logging: false,
+    // 	eventBusConfig: { enabled: false },
+    // }),
+    env.mode === "development" &&
+      jsxSource({
+        enforce: "pre",
+        transformFileName: (fileName, loc) => defaultTransformFileName(fileName, loc),
+      }),
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tailwindcss(),
+    tanstackStart({ spa: { enabled: false } }),
+    viteReact(),
+  ],
+}));
+
+export default config;
